ca3dcb95ec804d29576a74012342e2a7dba425c8
freetown18 <freetown18@gmail.com>
2022-09-02 18:38:34 +0300

    добавил поля и имена столбцов

--- src/main/java/com/kata/market_accounting/models/Currency.java
+++ src/main/java/com/kata/market_accounting/models/Currency.java
@@ -1,10 +1,48 @@
 package com.kata.market_accounting.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "is_accounting")
     private boolean isAccounting; //является ли валютой учета
+
+    @NonNull
+    @Column(name = "short_name")
     private String shortName;
+
+    @NonNull
+    @Column(name = "full_name")
     private String fullName;
+
+    @Column(name = "digital_code")
     private short digitalCode;
+
+    @NonNull
+    @Column(name = "letter_code")
     private String letterCode; // буквенный код
+
+    @Column(name = "exchange_rate")
     private double exchangeRate; // курс
 }
