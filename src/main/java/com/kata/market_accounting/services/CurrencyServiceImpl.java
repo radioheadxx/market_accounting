ecbbe14c61305708b68217d33decb0c47cf4eeaa
freetown18 <freetown18@gmail.com>
2022-09-05 17:59:03 +0300

    создал сервис-интерфейс и его имплементацию; исправил название папки репозитория; изменил RequestMapping в рестконтроллере; добавил аннотации для Сваггера

---
+++ src/main/java/com/kata/market_accounting/services/CurrencyServiceImpl.java
@@ -0,0 +1,49 @@
+package com.kata.market_accounting.services;
+
+import com.kata.market_accounting.models.Currency;
+import com.kata.market_accounting.repositories.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CurrencyServiceImpl implements CurrencyService {
+    private final CurrencyRepository currencyRepo;
+
+    @Autowired
+    public CurrencyServiceImpl(CurrencyRepository currencyRepo) {
+        this.currencyRepo = currencyRepo;
+    }
+
+    @Override
+    @Transactional
+    public void create(Currency currency){
+        currencyRepo.save(currency);
+    }
+
+    @Override
+    public List<Currency> getAll() {
+        return currencyRepo.findAll();
+    }
+
+    @Override
+    public Currency getOneById(long id) {
+        Optional<Currency> foundCurrency = currencyRepo.findById(id);
+        return foundCurrency.orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void update(Currency currency) {
+        currencyRepo.save(currency);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        currencyRepo.deleteById(id);
+    }
+}
