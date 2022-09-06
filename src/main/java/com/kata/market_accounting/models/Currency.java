b923697d456e6e45df89243d05d62e5d650453f9
freetown18 <freetown18@gmail.com>
2022-09-06 15:50:30 +0300

    Мелкие исправления/ minor fixes

--- src/main/java/com/kata/market_accounting/models/Currency.java
+++ src/main/java/com/kata/market_accounting/models/Currency.java
@@ -1,11 +1,9 @@
 package com.kata.market_accounting.models;
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,35 +12,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "currencies")
-@Getter
-@Setter
-@ToString
+@Data
 @RequiredArgsConstructor
 @NoArgsConstructor
 public class Currency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name="id", nullable = false)
     private Long id;
 
-    @Column(name = "is_accounting")
+    @Column(name="is_accounting")
     private Boolean isAccounting; //является ли валютой учета
 
     @NonNull
-    @Column(name = "short_name")
+    @Column(name="short_name")
     private String shortName;
 
     @NonNull
-    @Column(name = "full_name")
+    @Column(name="full_name")
     private String fullName;
 
-    @Column(name = "digital_code")
+    @Column(name="digital_code")
     private String digitalCode;
 
     @NonNull
-    @Column(name = "letter_code")
+    @Column(name="letter_code")
     private String letterCode; // буквенный код
 
-    @Column(name = "exchange_rate")
+    @Column(name="exchange_rate")
     private Double exchangeRate; // курс
 }
--- src/main/java/com/kata/market_accounting/services/CurrencyService.java
+++ src/main/java/com/kata/market_accounting/services/CurrencyService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface CurrencyService {
     void create(Currency currency);
+
     List<Currency> getAll();
+
     Currency getOneById(long id);
+
     void update(Currency currency);
+
     void delete(Long id);
 }
