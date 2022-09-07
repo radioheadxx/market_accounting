283ffe56197f1901c20203ee11dd184eef8c434f
freetown18 <freetown18@gmail.com>
2022-09-07 16:11:30 +0300

    Прописал дополнительные аннотации по Сваггеру

--- src/main/java/com/kata/market_accounting/repositories/CurrencyRepository.java
+++ src/main/java/com/kata/market_accounting/repositories/CurrencyRepository.java
@@ -2,11 +2,9 @@ package com.kata.market_accounting.repositories;
 
 import com.kata.market_accounting.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
-    @Query("SELECT c FROM currencies c WHERE c.shortName = ?1")
     Currency findByShortName(String shortName);
 }
--- src/main/java/com/kata/market_accounting/rest/CurrencyController.java
+++ src/main/java/com/kata/market_accounting/rest/CurrencyController.java
@@ -5,6 +5,9 @@ import com.kata.market_accounting.services.CurrencyService;
 import com.kata.market_accounting.services.CurrencyServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,42 +22,60 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/currency")
-@Api(tags = "Currency controller")
+@Api(value = "/api/currency", tags = "Currency controller", description = "Operations with currencies")
 public class CurrencyController {
-    private final CurrencyService service;
+    private final CurrencyService currencyService;
 
     @Autowired
-    public CurrencyController(CurrencyServiceImpl service) {
-        this.service = service;
+    public CurrencyController(CurrencyServiceImpl currencyService) {
+        this.currencyService = currencyService;
     }
 
     @PostMapping("/new")
-    @ApiOperation(value = "Create currency")
+    @ApiOperation(value = "Create a new currency", notes = "Creates a new currency", tags = {"Currency API"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully created")
+    })
     public void addNew(@RequestBody Currency currency) {
-        service.create(currency);
+        currencyService.create(currency);
     }
 
     @GetMapping
-    @ApiOperation(value = "Get all currencies")
+    @ApiOperation(value = "Get all currencies", notes = "Returns all currencies", tags = {"Currency API"})
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved")
+    })
     public List<Currency> getCurrencies() {
-        return service.getAll();
+        return currencyService.getAll();
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "Get one currency")
-    public Currency getCurrency(@PathVariable("id") long id) {
-        return service.getOneById(id);
+    @ApiOperation(value = "Get a currency by id", notes = "Returns a currency as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved"),
+            @ApiResponse(code = 404, message = "Not found - The currency was not found")
+    })
+    public Currency getCurrency(@PathVariable("id") @ApiParam(name = "id", value = "Currency id", example = "1") long id) {
+        return currencyService.getOneById(id);
     }
 
     @PutMapping
-    @ApiOperation(value = "Update currency")
+    @ApiOperation(value = "Update a currency", notes = "Updates a currency")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated"),
+            @ApiResponse(code = 404, message = "Not found - The currency was not found")
+    })
     public void updateCurrency(@RequestBody Currency currency) {
-        service.update(currency);
+        currencyService.update(currency);
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation("Delete currency")
-    public void delete(@PathVariable("id") long id) {
-        service.delete(id);
+    @ApiOperation(value = "Delete a currency by id", notes = "Deletes a currency as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted"),
+            @ApiResponse(code = 404, message = "Not found - The currency was not found")
+    })
+    public void delete(@PathVariable("id") @ApiParam(name = "id", value = "Currency id", example = "1") long id) {
+        currencyService.delete(id);
     }
 }
