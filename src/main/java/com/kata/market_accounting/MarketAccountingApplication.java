9c8ad6b095fe2a26855dc056efdc8d17fc3a5b6b
silvestrr <nikitamaliev000@gmail.com>
2022-09-09 08:52:51 +0300

    new

--- src/main/java/com/kata/market_accounting/MarketAccountingApplication.java
+++ src/main/java/com/kata/market_accounting/MarketAccountingApplication.java
@@ -4,14 +4,13 @@ import com.kata.market_accounting.models.Project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
-@EnableSwagger2
 @EnableWebMvc
 @SpringBootApplication
 @EnableSwagger2
@@ -26,4 +25,5 @@ public class MarketAccountingApplication {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.kata.market_accounting")).build();
     }
+
 }
