e2815e32575c87121fd99a557a7e51a291ca84c0
silvestrr <nikitamaliev000@gmail.com>
2022-09-14 16:14:47 +0300

    new request mapping

--- src/main/java/com/kata/market_accounting/rest/CountryRestController.java
+++ src/main/java/com/kata/market_accounting/rest/CountryRestController.java
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/countries")
 @Api(tags = "Country controller")
 public class CountryRestController {
     CountryServiceImpl countryService;
