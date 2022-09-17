79619e38552127a86ad3f0d51f3f2000a5473618
Alex Vasin <asentors@gmail.com>
2022-09-17 12:48:36 +0500

    InitWarehousesT

--- src/main/java/com/kata/market_accounting/init/InitWarehouses.java
+++ src/main/java/com/kata/market_accounting/init/InitWarehouses.java
@@ -7,8 +7,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-
-
 @Component
 public class InitWarehouses implements ApplicationRunner {
     private final WarehousesRepository warehousesRepository;
