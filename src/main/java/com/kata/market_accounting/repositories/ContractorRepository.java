655d9a08086c30e892eeef7c2de8d7a3465965cb
Andrei Efremov <freetown18@gmail.com>
2022-09-20 12:56:12 +0300

    Добавлены классы контрагента

---
+++ src/main/java/com/kata/market_accounting/repositories/ContractorRepository.java
@@ -0,0 +1,9 @@
+package com.kata.market_accounting.repositories;
+
+import com.kata.market_accounting.models.Contractor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractorRepository extends JpaRepository<Contractor, Long> {
+}
---
+++ src/main/java/com/kata/market_accounting/services/ContractorServiceImpl.java
@@ -0,0 +1,74 @@
+package com.kata.market_accounting.services;
+
+import com.kata.market_accounting.models.Contractor;
+import com.kata.market_accounting.repositories.ContractorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+public class ContractorServiceImpl implements ContractorService{
+    private final ContractorRepository contractorRepository;
+    private final ContractorStatusRepository statusRepository;
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+    @Autowired
+    public ContractorServiceImpl(ContractorRepository contractorRepository, ContractorStatusRepository statusRepository) {
+        this.contractorRepository = contractorRepository;
+        this.statusRepository = statusRepository;
+    }
+
+    @Override
+    public void create(Contractor contractor) {
+        if (contractor.getName() == null) {
+            throw new EmptyInputException("Название не может быть пустым!");
+        }
+        String formattedDateTime = LocalDateTime.now().format(dateTimeFormatter);
+        contractor.setCreatedAt(formattedDateTime);
+        contractor.setChangedAt(formattedDateTime);
+        contractor.setChangedWho("Test User");
+
+        ContractorStatus status = new ContractorStatus("Новый", "Обычный", ContractorStatus.Colors.ORANGE);
+        statusRepository.save(status);
+        contractor.setStatus(status);
+        contractorRepository.save(contractor);
+    }
+
+    @Override
+    public List<Contractor> getAll() {
+        if (contractorRepository.findAll().isEmpty()) {
+            throw new NoSuchElementException("Нет контрагентов");
+        }
+        return contractorRepository.findAll();
+    }
+
+    @Override
+    public Contractor getOneById(long id) {
+        return contractorRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Нет такого контрагента"));
+    }
+
+    @Override
+    public Contractor update(Contractor contractor) {
+        if (contractor.getName() == null) {
+            throw new EmptyInputException("Название не может быть пустым!");
+        }
+        String formattedDateTime = LocalDateTime.now().format(dateTimeFormatter);
+        contractor.setChangedAt(formattedDateTime);
+        contractor.setChangedWho("Test User");
+        return contractorRepository.save(contractor);
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (contractorRepository.findById(id).isEmpty()) {
+            throw new NoSuchElementException("Не могу удалить контрагента, так как контрагент не найден");
+        }
+        contractorRepository.deleteById(id);
+    }
+}
