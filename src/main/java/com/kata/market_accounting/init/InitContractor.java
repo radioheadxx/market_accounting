27e134f3b32d597868d4d5d68eb61928f4af27b9
Andrei Efremov <freetown18@gmail.com>
2022-09-20 14:50:42 +0300

    Добавлены физ и юр лица для заполнения БД

--- src/main/java/com/kata/market_accounting/init/InitContractor.java
+++ src/main/java/com/kata/market_accounting/init/InitContractor.java
@@ -21,17 +21,45 @@ public class InitContractor implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Contractor contractor = new Contractor();
+        Contractor contractor1 = new Contractor();
+        Contractor contractor2 = new Contractor();
+        Contractor contractor3 = new Contractor();
         String formattedDateTime = LocalDateTime.now().format(formatter);
-        contractor.setName("ООО Покупатель");
-        contractor.setStatus(new ContractorStatus("Новый", ContractorStatus.StatusDefinitions.USUAL.getDef(), ContractorStatus.Colors.ORANGE));
-        contractor.setContractorType("Юридическое лицо");
-        contractor.setCRR("773601002");
-        contractor.setOwnerEmployee("New employee 1");
-        contractor.setOwnerDept("Основной");
-        contractor.setCreatedAt(formattedDateTime);
-        contractor.setChangedAt(formattedDateTime);
-        contractor.setChangedWho("Test user 1");
-        contractorRepository.save(contractor);
+
+        contractor1.setName("ООО Покупатель");
+        contractor1.setStatus(new ContractorStatus("Новый", ContractorStatus.StatusDefinitions.USUAL.getDef(), ContractorStatus.Colors.ORANGE));
+        contractor1.setContractorType("Юридическое лицо");
+        contractor1.setCRR("773601002");
+        contractor1.setOwnerEmployee("New employee 1");
+        contractor1.setOwnerDept("Основной");
+        contractor1.setCreatedAt(formattedDateTime);
+        contractor1.setChangedAt(formattedDateTime);
+        contractor1.setChangedWho("New employee 1");
+
+        contractor2.setName("ООО Поставщик");
+        contractor2.setStatus(new ContractorStatus("Новый", ContractorStatus.StatusDefinitions.FINAL_POSITIVE.getDef(), ContractorStatus.Colors.BLUE));
+        contractor2.setFullName("Общество с ограниченной ответственностью \"Поставщик\"");
+        contractor2.setTIN("7736570901");
+        contractor1.setContractorType("Юридическое лицо");
+        contractor1.setCRR("773601001");
+        contractor2.setLegalAddress("г.Москва, ул.Строителей, д.12");
+        contractor1.setOwnerEmployee("New employee 2");
+        contractor1.setOwnerDept("Основной");
+        contractor1.setCreatedAt(formattedDateTime);
+        contractor1.setChangedAt(formattedDateTime);
+        contractor1.setChangedWho("New employee 2");
+
+        contractor2.setName("Розничный покупатель");
+        contractor1.setStatus(new ContractorStatus("Новый", ContractorStatus.StatusDefinitions.USUAL.getDef(), ContractorStatus.Colors.BROWN));
+        contractor1.setContractorType("Физическое лицо");
+        contractor1.setOwnerEmployee("New employee 1");
+        contractor1.setOwnerDept("Основной");
+        contractor1.setCreatedAt(formattedDateTime);
+        contractor1.setChangedAt(formattedDateTime);
+        contractor1.setChangedWho("Test user 1");
+
+        contractorRepository.save(contractor1);
+        contractorRepository.save(contractor2);
+        contractorRepository.save(contractor3);
     }
 }
