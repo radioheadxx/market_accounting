c81551398976b59a1ba0c5245b9a8c85df393221
Andrei Efremov <freetown18@gmail.com>
2022-09-22 12:57:08 +0300

    Тест сервис-слоя для Статуса контрагента

--- src/main/java/com/kata/market_accounting/models/ContractorStatus.java
+++ src/main/java/com/kata/market_accounting/models/ContractorStatus.java
@@ -2,19 +2,39 @@ package com.kata.market_accounting.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
 
 @Entity
+@Table(name = "status", schema = "market")
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class ContractorStatus {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NonNull
     private String description;
-    private StatusDefinitions definition;
+
+    @NonNull
+    private String definition;
+
+    @NonNull
     private Colors color;
+
+    @OneToMany(mappedBy = "status", cascade = CascadeType.ALL)
+    private List<Contractor> contractors;
 
     public enum StatusDefinitions {
         USUAL("Обычный"),
@@ -25,6 +45,10 @@ public class ContractorStatus {
 
         StatusDefinitions(String def) {
             this.def = def;
+        }
+
+        public String getDef() {
+            return def;
         }
     }
 
