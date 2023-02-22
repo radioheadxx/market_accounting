402f4c66c3be0eaf28ac7614fc977ba985540210
radioheadxx <holdenkolfild@inbox.ru>
2023-02-22 17:58:08 +0300

    /

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into legal_entities(code, comment, email, physical_address, public_access, short_name, telephone)
-values (101, 'We have four branches near the metro', 'tutryadom@mail.ru', null, true, 'Tutryadom', '8-499-387-11-12'),
-       (102, null, 'zvezda1@gmail.com', 'Moscow, Lesovaya, 10', false, 'Zvezda', '8-954-895-55-20'),
-       (103, 'additional number - 8-499-145-98-61', 'karomovoleg67@yandex.ru', null, true, 'Oleg Karomov', '8-999-120-40-30'),
-       (104, 'Use email to communicate', 'kitchenaccess@mail.ru', 'Moscow, Dorozhnaya, 128', true, 'Kitch', null),
-       (105, null, '89999999999@mail.ru', null, true, 'Elka', '8-999-999-99-99');
+values (101, 'We have four branches near the metro', 'tutryaadom@mail.ru', null, true, 'Tutryadom', '8-499-387-11-12'),
+       (102, null, 'zvezdaa1@gmail.com', 'Moscow, Lesovaya, 10', false, 'Zvezda', '8-954-895-55-20'),
+       (103, 'additional number - 8-499-145-98-61', 'kaaromovoleg67@yandex.ru', null, true, 'Oleg Karomov', '8-999-120-40-30'),
+       (104, 'Use email to communicate', 'kitchenaaccess@mail.ru', 'Moscow, Dorozhnaya, 128', true, 'Kitch', null),
+       (105, null, '89999999999a@mail.ru', null, true, 'Elka', '8-999-999-99-99');
 
 INSERT INTO currencies(is_accounting, short_name, full_name, digital_code, letter_code, exchange_rate)
 VALUES (FALSE, 'евро', 'Евро', '978', 'EUR', '60.65'),
